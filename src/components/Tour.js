7afd86bf317a595f5d3a78c34abbfe8280cabeab
unknown <minwidth49@gmail.com>
2024-06-27 10:24:33 +0000

    minor changes

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -6,11 +6,13 @@ const TourComponent = ({ runTour, setRunTour }) => {
   const steps = [
     {
       target: '#Sermons1',
-      content: 'This Opens a random Sermon from The Prophet of God. A second click will open the side bar for  sermon selection',
+      content:
+        'This Opens a random Sermon from The Prophet of God. A second click will open the side bar for  sermon selection',
     },
     {
       target: '#Sermons',
-      content: 'This Opens a random Sermon from The Prophet of God. A second click will open the side bar for  sermon selection',
+      content:
+        'This Opens a random Sermon from The Prophet of God. A second click will open the side bar for  sermon selection',
     },
     {
       target: '#year',
@@ -32,10 +34,11 @@ const TourComponent = ({ runTour, setRunTour }) => {
       target: '#media',
       content: 'See a view images and videos of the Prophet of God',
     },
-   
+
     {
       target: '#settings',
-      content: 'Change the font size, font family, background color and text color of the sermon text',
+      content:
+        'Change the font size, font family, background color and text color of the sermon text',
     },
     {
       target: '#home',
@@ -45,6 +48,14 @@ const TourComponent = ({ runTour, setRunTour }) => {
 
   return (
     <Joyride
+      styles={{
+        options: {
+          arrowColor: '#fafafa',
+          backgroundColor: '#fafafa',
+          overlayColor: 'rgba(79, 26, 0, 0.4)',
+          primaryColor: '#40aae2',
+        },
+      }}
       steps={steps}
       run={runTour}
       continuous
